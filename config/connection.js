491fac2d677303ed0382b4b505b8d4738ef83a60
William Broussard <wbroussard@me.com>
2020-07-24 23:30:31 -0500

    connection updates

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,13 @@ const mysql = require("mysql");
 // dotenv needed to avoid posting password in public
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const connection
+
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
